47b26ff944ea3ba26d9fd1b03551da59af01f6f1
jadier <144461536+jadierb@users.noreply.github.com>
2026-01-15 01:29:21 +0000

    added images URLs

--- script.js
+++ script.js
@@ -3,11 +3,26 @@ var spring = ["March", "April", "May"];
 var summer = ["June", "July", "August"];
 var fall = ["September", "October", "November"];
 
-var winterImages;
-var springImages;
-var summerImages;
-var fallImages;
-
+var winterImages = [
+	"https://images.unsplash.com/photo-1518837695005-2083093ee35b?w=300",
+	"https://images.unsplash.com/photo-1506744038136-46273834b3fb?w=300",
+	"https://images.unsplash.com/photo-1486308510493-cb6f5d2fif3b?w=300",
+]
+var springImages = [
+	"https://images.unsplash.com/photo-1518837695005-2083093ee35b?w=300",
+	"https://images.unsplash.com/photo-1506744038136-46273834b3fb?w=300",
+	"https://images.unsplash.com/photo-1486308510493-cb6f5d2fif3b?w=300",
+]
+var summerImages = [
+	"https://images.unsplash.com/photo-1518837695005-2083093ee35b?w=300",
+	"https://images.unsplash.com/photo-1506744038136-46273834b3fb?w=300",
+	"https://images.unsplash.com/photo-1486308510493-cb6f5d2fif3b?w=300",
+]
+var fallImages = [
+	"https://images.unsplash.com/photo-1518837695005-2083093ee35b?w=300",
+	"https://images.unsplash.com/photo-1506744038136-46273834b3fb?w=300",
+	"https://images.unsplash.com/photo-1486308510493-cb6f5d2fif3b?w=300",
+];
 
 
 function displayWinter() {
